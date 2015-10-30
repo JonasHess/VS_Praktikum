ac4c0c2f60d8067bd8ba1ab364f2485935af556f
OliverNeff <OliNeff@aol.com>
2015-10-30 16:36:00 +0100

    Update SmartCar.java

--- Eclipse_Projekt/src/de/h_da/VS/Praktikum/Cars/SmartCar.java
+++ Eclipse_Projekt/src/de/h_da/VS/Praktikum/Cars/SmartCar.java
@@ -1,6 +1,10 @@
 package de.h_da.VS.Praktikum.Cars;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.newdawn.slick.geom.Vector2f;
 
@@ -9,6 +13,7 @@ import de.h_da.VS.Praktikum.Graph.Node;
 import de.h_da.VS.Praktikum.Transmitter.TCP_Transmitter;
 import de.h_da.VS.Praktikum.Transmitter.Transmitter;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import sun.security.provider.certpath.Vertex;
 
 public class SmartCar extends Car{
 
@@ -39,6 +44,54 @@ public class SmartCar extends Car{
 		super.delete();
 	}
 	
+	
+	public Node minVertex(Map<Node, Float> dist, Map<Node,Boolean> visited) {
+		float x = Float.MAX_VALUE;
+		Node bestNode = null;
+		for (Map.Entry<Node, Float> d : dist.entrySet()) {
+			if (!visited.get(d.getKey()) && d.getValue() < x) {
+				bestNode = d.getKey();
+			}
+		}
+		
+		return bestNode;
+	}
+
+	
+	public Node dijkstra(List<Node> graph, Node myNode) {
+		final Map<Node, Float> dist = new HashMap<>();
+		final List<Node> pred = new ArrayList<Node>();
+		final Map<Node,Boolean> visited = new HashMap<>();
+		
+		for (Node n : graph) {
+			if (n.getId() == myNode.getId()) {
+			dist.put(n, 0.0f);
+			visited.put(n, false);
+			} else {
+			dist.put(n, Float.MAX_VALUE);
+			visited.put(n, false);
+			}
+		}
+	
+		for(Node node : graph) {
+			final Node next = minVertex(dist, visited);
+			visited.put(next,true);
+		
+			final List<Node> neighbors = next.getNeighboursNodes();
+			for (Node v  : neighbors) {
+				final float d = dist.get(next) + next.getEdges(v).getCost(); 
+				if (dist.get(v) > d) {
+					dist.put(v, d);
+					pred.add(next);
+				}
+			}
+		}
+		
+		return pred.get(0); // for edge myNode.getEdges(pred.get(0));
+	}
+	
+	
+	
 	/**
 	 * Calculates the direction of the next turn. 
 	 * @return
@@ -47,13 +100,14 @@ public class SmartCar extends Car{
 	protected Edge findNextDestination() {
 		
 		Node finalDestination = destination; 
-		Node curretPosition = currentEdge.getDestinationNode();
+		Node currentPosition = currentEdge.getDestinationNode();
+		
+		
+		
 		
 		
 		// TODO  calculate shortest path to destination;
-
-		
-		List<Edge> outgoingEdges = curretPosition.getEdges();
+		List<Edge> outgoingEdges = currentPosition.getEdges();
 		float cost1 = outgoingEdges.get(0).getCost();
 		Node nextNode1 = outgoingEdges.get(0).getDestinationNode(); 
 
