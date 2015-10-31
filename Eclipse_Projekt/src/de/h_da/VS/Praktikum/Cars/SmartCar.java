1fe92c5b24ad27ea6802c9505530761ae2841de6
OliverNeff <OliNeff@aol.com>
2015-10-31 17:31:01 +0100

    Update SmartCar.java
    Probleme beim integrieren, aber algorythmus sollte nun passen.

--- Eclipse_Projekt/src/de/h_da/VS/Praktikum/Cars/SmartCar.java
+++ Eclipse_Projekt/src/de/h_da/VS/Praktikum/Cars/SmartCar.java
@@ -18,6 +18,7 @@ import sun.security.provider.certpath.Vertex;
 public class SmartCar extends Car {
 
 	private Transmitter transmitter;
+	private List<Node> nodesList;
 
 	/**
 	 * Construcor
@@ -27,8 +28,10 @@ public class SmartCar extends Car {
 	 * @param destination
 	 * @param maxSpeed
 	 */
-	public SmartCar(Edge currentEdge, Vector2f spawnPoint, Node destination, float maxSpeed, List<Node> graph) {
-		super(currentEdge, spawnPoint, destination, maxSpeed, graph);
+	public SmartCar(Edge currentEdge, Vector2f spawnPoint, Node destination, float maxSpeed, List<Node> nodesList) {
+		super(currentEdge, spawnPoint, destination, maxSpeed);
+
+		this.nodesList = nodesList;
 
 		this.transmitter = new TCP_Transmitter(this);
 		transmitter.start();
@@ -56,36 +59,41 @@ public class SmartCar extends Car {
 		return bestNode;
 	}
 
-	public Node dijkstra(List<Node> graph, Node myNode) {
+	public Map<Node, List<Node>> dijkstra(List<Node> graph, Node myNode) {
 		final Map<Node, Float> dist = new HashMap<>();
-		final List<Node> pred = new ArrayList<Node>();
+		final Map<Node, List<Node>> pred = new HashMap<>();
 		final Map<Node, Boolean> visited = new HashMap<>();
 
 		for (Node n : graph) {
 			if (n.getId() == myNode.getId()) {
 				dist.put(n, 0.0f);
 				visited.put(n, false);
+				pred.put(n, new ArrayList<Node>());
 			} else {
 				dist.put(n, Float.MAX_VALUE);
 				visited.put(n, false);
+				pred.put(n, new ArrayList<Node>());
 			}
 		}
 
-		for (Node node : graph) {
+		for (int i = 0; i < graph.size(); i++) {
 			final Node next = minVertex(dist, visited);
+			
 			visited.put(next, true);
-
+			if (next == null) {
+				continue;
+			}
 			final List<Node> neighbors = next.getNeighboursNodes();
 			for (Node v : neighbors) {
 				final float d = dist.get(next) + next.getEdges(v).getCost();
 				if (dist.get(v) > d) {
 					dist.put(v, d);
-					pred.add(next);
+					pred.get(v).add(next);
 				}
 			}
 		}
 
-		return pred.get(0); // for edge myNode.getEdges(pred.get(0));
+		return pred;
 	}
 
 	/**
@@ -95,10 +103,17 @@ public class SmartCar extends Car {
 	 */
 	@Override
 	protected Edge findNextDestination() {
-
-		Node finalDestination = destination;
-		Node currentPosition = currentEdge.getDestinationNode();
-
+		Node myNode = getCurrentEdge().getDestinationNode();
+		
+		Map<Node, List<Node>> result = dijkstra(nodesList, myNode);
+		Node nextNode = result.get(destination).get(0);
+		
+		
+		System.out.println(nextNode.getId());
+		
+		
+		
+		return myNode.getEdges(nextNode);
 	}
 
 }
