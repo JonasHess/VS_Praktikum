9f58e5af2061be22057c2f6151afea75fadef276
sebastian1102 <sebastian.g1993.sg@gmail.com>
2015-11-16 19:00:53 +0100

    Update Monitor.java

--- Monitor/src/de/hda/VSPraktikum/Monitor.java
+++ Monitor/src/de/hda/VSPraktikum/Monitor.java
@@ -4,6 +4,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 import de.hda.VSPraktikum.Cars.Car;
+import static java.lang.Float.NaN;
+import java.util.Iterator;
 
 public class Monitor {
 	private List<Car> connectedCars;
@@ -14,16 +16,75 @@ public class Monitor {
 		this.id = id;
 		this.connectedCars = new LinkedList<Car>();
 	}
-	
 	public synchronized void addCar(Car car) {
-		if (! connectedCars.contains(car)) {
-			this.connectedCars.add(car);
-		}
-		// TODO throw exception when currentEdge of car is != Monitor id
+            int index = connectedCars.indexOf(car);  //Notiz: equals ist in Car überladen     
+            if (index == -1) {
+                    this.connectedCars.add(car);
+            }
+            else {
+                    this.connectedCars.remove(index);
+                    this.addCar(car);
+            }
+            // TODO throw exception when currentEdge of car is != Monitor id
 	}
-	
+	public synchronized boolean containsCar(Car car) {
+            return connectedCars.contains(car);
+        }
 	public synchronized void removeCar(Car car) {
 		this.connectedCars.remove(car);
+	}
+	public synchronized void removeCar(int index) {
+            this.connectedCars.remove(index);
+        }
+        public synchronized Car getSameCar(Car car) {
+            int index = connectedCars.indexOf(car);
+            if(index != -1) {
+                return connectedCars.get(index);
+            }
+            return null;
+        }
+        	
+	public synchronized void printStatus() {  
+            boolean trafficJam = false;
+            float averageSpeed = 0.0f;
+            long averageUpdateInterval = 0;
+            int numOfValidAvgSpeed = 0;
+            int numOfValidAvgUpdate = 0;
+            
+            //Kick old cars out
+            for(Iterator<Car> c = connectedCars.iterator(); c.hasNext(); ) {
+                Car car = c.next();
+                if(System.currentTimeMillis()-car.getLastTimeStamp() > 2000) {
+                    c.remove();
+                }                    
+            }
+
+            for (Car c : this.connectedCars) {   
+                    float cAvgSpeed = c.getAverageSpeed();
+                    long cAvgUpdate = c.getUpdateInterval();
+                    if(cAvgSpeed != NaN && cAvgSpeed != -1.0f) {
+                        averageSpeed += c.getAverageSpeed();
+                        numOfValidAvgSpeed++;
+                    }
+                    if(cAvgUpdate != -1) {
+                        averageUpdateInterval += c.getUpdateInterval();
+                        numOfValidAvgUpdate++;
+                    }
+                    trafficJam = trafficJam || c.isInTrafficJam();
+            }
+            if(numOfValidAvgSpeed > 0) {
+                averageSpeed = averageSpeed / numOfValidAvgSpeed;
+            }
+            else {
+                averageSpeed = -1.0f;
+            }
+            if(numOfValidAvgUpdate > 0) {
+                averageUpdateInterval = averageUpdateInterval / numOfValidAvgUpdate;
+            }
+            else {
+                averageUpdateInterval = -1;
+            }
+            System.out.println("Connected Cars: " + connectedCars.size() + " Stau: " + trafficJam + " AvgSpeed: " + averageSpeed + " AvgUpdate: " + averageUpdateInterval);
 	}
 	
 //	 public static float getCosts() {       
@@ -53,22 +114,5 @@ public class Monitor {
 //	            */
 //	            return costs;
 //	        }        
-//	    }
-	 
-	
-	public synchronized void printStatus() {
-		boolean trafficJam = false;
-		float averageSpeed = -1.0f;
-		for (Car c : this.connectedCars) {
-			averageSpeed += c.getAverageSpeed();
-			trafficJam = trafficJam || c.isInTrafficJam();
-		}
-		if (this.connectedCars.size() > 0) {
-			averageSpeed = averageSpeed / this.connectedCars.size();
-		}
-		
-		System.out.println("Connected Cars: " + connectedCars.size() + " Stau: " + trafficJam + " AvgSpeed: " + averageSpeed);
-	}
-	
-	
+//	    }	
 }
