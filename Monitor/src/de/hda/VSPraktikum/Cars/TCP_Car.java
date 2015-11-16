1abe038b6961ee18e24cfc759a65ac04bce9412a
sebastian1102 <sebastian.g1993.sg@gmail.com>
2015-11-16 19:02:42 +0100

    Update TCP_Car.java

--- Monitor/src/de/hda/VSPraktikum/Cars/TCP_Car.java
+++ Monitor/src/de/hda/VSPraktikum/Cars/TCP_Car.java
@@ -8,27 +8,55 @@ import de.hda.VSPraktikum.Monitor;
 
 public class TCP_Car extends Car {
 
-	private final Socket socket;
-	private BufferedReader fromClient;
+    private final Socket socket;
+    private BufferedReader fromClient;
 
-	public TCP_Car(Socket client, Monitor monitor) {
-		super(monitor);
-		this.socket = client;
-	}
-	
-	@Override
-	protected void initializeConnection() throws Exception {
-		fromClient = new BufferedReader(new InputStreamReader(this.socket.getInputStream())); // Datastream
-	}
-	
-	@Override
-	protected boolean isConnected() {
-		return this.socket != null && this.socket.isConnected();
-	}
-	
-	@Override
-	protected String readNextLine() throws Exception {
-		return fromClient.readLine();
-	}
+    public TCP_Car(Socket client, Monitor monitor) {
+            super(monitor);
+            this.socket = client;
+    }
+        
+    @Override
+    public void run() {
+        try {
+            this.initializeConnection();
+            String line;
+            while(this.isConnected()) {
+            	line =  this.readNextLine();
+                if(line == null || line.length() == 0 || line.equals("bye")) {
+                    break;
+                }
+                // System.out.println("Received: "+ line);
+                try {
+                	parseLine(line);
+                        this.setNewTimeStamp();
+                        if(!monitor.containsCar(this))
+                            this.monitor.addCar(this);
+                
+                } catch (Exception e) {
+                	System.out.println(e);
+                	continue;
+                }
+            }
+        }
+        catch(Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        } finally {
+        	this.monitor.removeCar(this);
+        }
+    }        
+
+    protected void initializeConnection() throws Exception {
+            fromClient = new BufferedReader(new InputStreamReader(this.socket.getInputStream())); // Datastream
+    }
+
+    protected boolean isConnected() {
+            return this.socket != null && this.socket.isConnected();
+    }
+
+    protected String readNextLine() throws Exception {
+            return fromClient.readLine();
+    }
 
 }
