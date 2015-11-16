3643bae17db4eb2496fff44ffdb8072c74dc0365
sebastian1102 <sebastian.g1993.sg@gmail.com>
2015-11-16 19:01:58 +0100

    Update Car.java

--- Monitor/src/de/hda/VSPraktikum/Cars/Car.java
+++ Monitor/src/de/hda/VSPraktikum/Cars/Car.java
@@ -12,92 +12,89 @@ import de.hda.VSPraktikum.Monitor;
 
 public abstract class Car extends Thread {
     
-	private Monitor monitor;
+    protected Monitor monitor;
 	
     private int id;
     private boolean isInTrafficJam;
     private float averageSpeed;
     private String currentEdge; 
     
-  
+    protected long timeStamp = 0;
+    protected long updateInterval;
     
     public Car(Monitor monitor) {
         this.monitor = monitor;
     }
-    
-    protected abstract void initializeConnection () throws Exception;
-    
-    protected abstract boolean isConnected();
-    
-    protected abstract String readNextLine() throws Exception;
-    @Override
-    public void run() {
-        try {
-            this.initializeConnection();
-            String line;
-            while(this.isConnected()) {
-            	line =  this.readNextLine();
-                if(line == null || line.length() == 0 || line.equals("bye")) {
-                    break;
-                }
-                // System.out.println("Received: "+ line);
-                try {
-                	parseLine(line);
-                	this.monitor.addCar(this);
-                } catch (Exception e) {
-                	System.out.println(e);
-                	continue;
-                }
-            }
-        }
-        catch(Exception e) {
-            System.out.println(e);
-            e.printStackTrace();
-        } finally {
-        	this.monitor.removeCar(this);
-        }
+
+    protected void parseLine(String line) throws Exception {
+        String[] fragments = line.split(";");
+        
+        this.id = Integer.parseInt(fragments[0]);
+        this.averageSpeed = Float.parseFloat(fragments[1]);
+        this.isInTrafficJam = Boolean.parseBoolean(fragments[2]);
+        this.currentEdge = fragments[3];
+        
+        
     }
     
-   private void parseLine (String line) throws Exception {
-	   String[] fragments = line.split(";");
-	   
-	   this.id = Integer.parseInt(fragments[0]);
-	   this.averageSpeed = Float.parseFloat(fragments[1]);
-	   this.isInTrafficJam = Boolean.parseBoolean(fragments[2]);
-	   this.currentEdge = fragments[3];
-       
-       
-   }
+    @Override
+    public boolean equals(Object obj) {
+    	if (this == obj)
+    		return true;
+    	if (obj == null)
+    		return false;
+    	if (getClass() != obj.getClass())
+    		return false;
+        Car otherCar = (Car)obj;
+    	if (id != otherCar.getCarId()) {
+    		return false;
+    	}
+        return true;
+    }
     
     @Override
-	public String toString() {
-		StringBuilder b = new StringBuilder();
-		b.append("id: " + id);
-		b.append( "averageSpeed: " + averageSpeed);
-		b.append( "isInTrafficJam: " + isInTrafficJam);
-		b.append( "currentEdge: " + currentEdge);
-		return b.toString();
-	}
+    public String toString() {
+            StringBuilder b = new StringBuilder();
+            b.append("id: " + id);
+            b.append( "averageSpeed: " + averageSpeed);
+            b.append( "isInTrafficJam: " + isInTrafficJam);
+            b.append( "currentEdge: " + currentEdge);
+            return b.toString();
+    }
 
-	public int getCarId() {
-		return id;
-	}
+    public int getCarId() {
+            return id;
+    }
 
-	public boolean isInTrafficJam() {
-		return isInTrafficJam;
-	}
+    public boolean isInTrafficJam() {
+            return isInTrafficJam;
+    }
 
-	public float getAverageSpeed() {
-		return averageSpeed;
-	}
+    public float getAverageSpeed() {
+            return averageSpeed;
+    }
 
-	public String getCurrentEdge() {
-		return currentEdge;
-	}
+    public String getCurrentEdge() {
+            return currentEdge;
+    }
 
-	public Monitor getMonitor() {
-		return monitor;
-	}
-    
-    
+    public Monitor getMonitor() {
+            return monitor;
+    }
+    public long getLastTimeStamp() {
+        return timeStamp;
+    }
+    public long getUpdateInterval() {
+        return updateInterval;
+    }
+    public void setNewTimeStamp() {
+        long newTimeStamp = System.currentTimeMillis();
+        if(this.timeStamp != 0)
+            this.updateInterval = newTimeStamp - this.timeStamp;
+        else
+            this.updateInterval = -1;
+        //System.out.println("New:" + newTimeStamp + " Old:" + timeStamp + " Update:" + updateInterval);
+        this.timeStamp = newTimeStamp;
+        
+    }
 }
