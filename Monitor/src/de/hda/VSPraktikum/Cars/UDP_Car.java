3a5e53f0613e2be2a53baec5acd0b4475c149310
sebastian1102 <sebastian.g1993.sg@gmail.com>
2015-11-19 10:12:00 +0100

    Update UDP_Car.java

--- Monitor/src/de/hda/VSPraktikum/Cars/UDP_Car.java
+++ Monitor/src/de/hda/VSPraktikum/Cars/UDP_Car.java
@@ -6,25 +6,30 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class UDP_Car extends Car{
-    
+    private DatagramPacket packet;
+    private Monitor monitor;
     public UDP_Car(DatagramPacket packet, Monitor monitor) {
         super(monitor);  
+        this.packet = packet;
+        this.monitor = monitor;
+
+    }
+    @Override
+    public void run() {
         try {
             parseLine(new String(packet.getData()));
             Car oldCar = monitor.getSameCar(this);
             this.setNewTimeStamp();
-            if(oldCar != null)
+            if(oldCar != null) {
                 this.updateInterval = this.timeStamp-oldCar.timeStamp;
+                //System.out.println(updateInterval);
+            }
             else
                 this.updateInterval = -1;
             monitor.addCar(this);
         } catch (Exception e) {
             System.out.println(e);
-        }
-    }
-    @Override
-    public void run() {
-        
+        }        
     }    
 
 }
