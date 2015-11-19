f2744fbbc42fadefb67227dc2106b57aceb3f56f
sebastian1102 <sebastian.g1993.sg@gmail.com>
2015-11-19 10:12:58 +0100

    Update TCP_ConnectionHandler.java

--- Monitor/src/de/hda/VSPraktikum/ConnectionHandlers/TCP_ConnectionHandler.java
+++ Monitor/src/de/hda/VSPraktikum/ConnectionHandlers/TCP_ConnectionHandler.java
@@ -1,10 +1,10 @@
 package de.hda.VSPraktikum.ConnectionHandlers;
 
+import de.hda.VSPraktikum.Cars.TCP_Car;
+import de.hda.VSPraktikum.Monitor;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import de.hda.VSPraktikum.Monitor;
-import de.hda.VSPraktikum.Cars.TCP_Car;
+import java.net.SocketException;
 
 public class TCP_ConnectionHandler extends ConnectionHandler {
 
@@ -20,21 +20,29 @@ public class TCP_ConnectionHandler extends ConnectionHandler {
 
 	@Override
 	protected void startListening(int port, Monitor monitor) throws Exception {
+            
 		ServerSocket listenSocket = null;
-		try {
-			listenSocket = new ServerSocket(port);
-			System.out.println("Multithreaded Server starts on Port " + port);
-			while (true) {
-				Socket client = listenSocket.accept();
-				//System.out.println("\n New Connection with: " + client.getRemoteSocketAddress() + "\n");
-				new TCP_Car(client, monitor).start();
-			}
-		} catch (Exception e) {
-			throw e;
-		} finally {
-			if (listenSocket != null) {
-				listenSocket.close();
-			}
-		}
+                while (true) {
+                
+                    try {
+                            listenSocket = new ServerSocket(port);
+                            System.out.println("Multithreaded Server starts on Port " + port);
+                            while (true) {
+                                    Socket client = listenSocket.accept();
+                                    //System.out.println("\n New Connection with: " + client.getRemoteSocketAddress() + "\n");
+                                    new TCP_Car(client, monitor).start();
+                            }
+                    } catch (SocketException e) {
+                        e.printStackTrace();
+                     
+                        continue;
+                    } catch (Exception e) {
+                            throw e;
+                    } finally {
+                            if (listenSocket != null) {
+                                    listenSocket.close();
+                            }
+                    }
+                }
 	}
 }
